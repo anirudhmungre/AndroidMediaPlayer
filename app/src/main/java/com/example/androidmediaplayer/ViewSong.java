71e5abf78ab7fae7126b64e3cf27411af96796f5
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-31 00:32:21 -0400

    working media buttons

--- app/src/main/java/com/example/androidmediaplayer/ViewSong.java
+++ app/src/main/java/com/example/androidmediaplayer/ViewSong.java
@@ -1,6 +1,7 @@
 package com.example.androidmediaplayer;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +10,14 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewSong extends AppCompatActivity {
-
     EditText title;
     EditText url;
     Button back;
     Intent intent;
     SongsDB songsDB;
     String old_title;
+    MediaPlayer mediaPlayer;
+    int playbackPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,49 @@ public class ViewSong extends AppCompatActivity {
         song = songsDB.getSong(old_title);
         title.setText(song.getTitle());
         url.setText(song.getUrl());
+        playbackPosition = 0;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        killMediaPlayer();
+    }
+
+    public void play(View v) {
+            if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+                mediaPlayer.seekTo(playbackPosition);
+                mediaPlayer.start();
+            } else if (!mediaPlayer.isPlaying()) {
+                try {
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(url.getText().toString());
+                    mediaPlayer.prepareAsync();
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mediaPlayer.start();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+    }
+
+    public void pause(View v) {
+        if(mediaPlayer != null && mediaPlayer.isPlaying()) {
+            playbackPosition = mediaPlayer.getCurrentPosition();
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stop(View v) {
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            killMediaPlayer();
+            playbackPosition = 0;
+        }
     }
 
     public void save(View v) {
@@ -48,5 +93,17 @@ public class ViewSong extends AppCompatActivity {
     public void back(View v) {
         setResult(RESULT_OK,intent);
         finish();
+    }
+
+    private void killMediaPlayer() {
+        if(mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
